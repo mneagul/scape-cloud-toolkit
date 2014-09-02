8ae5ef49df7bba58b97f0a83d290dd02a4959fe1
Marian Neagul <marian@ieat.ro>
2014-09-02 15:11:54 +0300

    Try to reduce userdata

--- src/sct/resources/puppet/bootstrap_master.pp
+++ src/sct/resources/puppet/bootstrap_master.pp
@@ -1,7 +1,2 @@
-class { 'puppetdb':
-  listen_address => "$ec2_local_ipv4"
-}
-class {'puppetdb::master::config':
-  puppetdb_server => "$ec2_local_ipv4",
-}
-
+class { 'puppetdb': listen_address => "$ec2_local_ipv4" }
+class {'puppetdb::master::config': puppetdb_server => "$ec2_local_ipv4", }
--- src/sct/templates/resources/setup_puppet_agent.sh
+++ src/sct/templates/resources/setup_puppet_agent.sh
@@ -1,17 +1,11 @@
 #!/bin/sh
 
-#
-# This script registers 'puppet' in /etc/hosts
-#
 . /etc/profile
-
 grep "puppet" /etc/hosts || echo "@puppetServer puppet" >> /etc/hosts
 echo -e "START=yes\nDAEMON_OPTS=\"\"\n" > /etc/default/puppet
-
 rm -fr /var/lib/puppet/ssl/*
 /etc/init.d/puppet start
-
-SWAP_DISKS=$(blkid -s TYPE | grep -i swap | cut -d ":" -f 1)
-for DSK in $SWAP_DISKS; do
-    swapon $DSK
+SD=$(blkid -s TYPE | grep -i swap | cut -d ":" -f 1)
+for D in $SD; do
+    swapon $D
 done
