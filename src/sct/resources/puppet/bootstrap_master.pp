511a4a1bfa21232c1c7e231159f68e27fde33010
Marian Neagul <marian@info.uvt.ro>
2014-11-04 22:09:38 +0200

    Fix puppet master PuppetDB issue

--- src/sct/resources/puppet/bootstrap_master.pp
+++ src/sct/resources/puppet/bootstrap_master.pp
@@ -1,5 +1,2 @@
-class { 'puppetdb':
-  listen_address => "0.0.0.0",
-  ssl_listen_address => "$ec2_local_hostname",
-}
-class {'puppetdb::master::config': puppetdb_server => "$ec2_local_hostname"}
+class{'puppetdb':listen_address => "0.0.0.0",ssl_listen_address => "$ec2_local_hostname"}
+class{'puppetdb::master::config': puppetdb_server => "$ec2_local_hostname"}
